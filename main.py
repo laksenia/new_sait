fd2028472e4419f23c659b98d4ebc8425458216d
User <liubov.ponomareva514@mail.ru>
2023-06-09 11:21:17 +0300

    all

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def add_bg_from_local(image_file):
     unsafe_allow_html=True
     )
 
-add_bg_from_local("photo1686134773.jpeg")
+add_bg_from_local("bg2.jpeg")
 
 
 
@@ -35,14 +35,12 @@ else:
     st.write('Ответ неверен')
 
 question2 = st.radio("От чего зависит цвет глаз ? ",
-    ('гинетика ', 'рандом ', 'нет точного ответа '    'Ни один ответ не верен'))
-if question2 == 'гинетика ':
+    ('генетика ', 'рандом ', 'нет точного ответа '    'Ни один ответ не верен'))
+if question2 == 'генетика ':
     st.write('Ответ верен')
     right_quest += 1
 else:
     st.write('Ответ неверен')
-
-st.write('Правильных ответов:', right_quest)
 
 
 question3 = st.radio("При смешании каких цветов не получиться зеленые глаза? ",
@@ -53,13 +51,61 @@ if question3 == 'голубой-коричневый':
 else:
     st.write('Ответ неверен')
 
-st.write('Правильных ответов:', right_quest)
-question4 = st.radio("сколько цветов видит человек?",
-    ('50k', '500k', '1 мил'))
-if question4 == '1 мил  ':
+question4 = st.radio("какую максимальную температуру выдерживае человек ?",
+    ('59˚С', '170-180˚С', '130˚С'))
+if question4 == '170-180˚С':
     st.write('Ответ верен')
     right_quest += 1
 else:
     st.write('Ответ неверен')
 
-st.write('Правильных ответов:', right_quest)
+question5 = st.radio("с какой скоростью чихает человек ?",
+                     ('100 km/h', '190km/h', '160km/h'))
+if question5 == '160km/h':
+    st.write('Ответ верен')
+    right_quest += 1
+else:
+    st.write('Ответ неверен')
+
+
+question6 = st.radio("сколько весит кожа средниго мужчины ?",
+                     ('9kg', '15kg', '5kg'))
+if question6 == '5kg':
+    st.write('Ответ верен')
+    right_quest += 1
+else:
+    st.write('Ответ неверен')
+
+question7 = st.radio("во сколько раз кость человека прочнее бетона ?",
+                     ('x6', 'x10', 'x4'))
+if question7 == 'x4':
+    st.write('Ответ верен')
+    right_quest += 1
+else:
+    st.write('Ответ неверен')
+question8 = st.radio("какое количество запохов может помнить человек?",
+                     ('70k', '150k', '50k'))
+if question8 == '50k':
+    st.write('Ответ верен')
+    right_quest += 1
+else:
+    st.write('Ответ неверен')
+question9 = st.radio("сколько km нервов в теле человека?",
+                     ('140km',' 200km ', '72km'))
+if question9 == '72km':
+    st.write('Ответ верен')
+    right_quest += 1
+else:
+    st.write('Ответ неверен')
+question10 = st.radio("из скольки костей состоит череп человека  ?",
+                     ('29','30', '47'))
+if question10 == '29':
+    st.write('Ответ верен')
+    right_quest += 1
+else:
+    st.write('Ответ неверен')
+
+
+
+
+st.write("Количество верных отвтов: ", right_quest)
